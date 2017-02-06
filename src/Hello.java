292c6ddf3adda8a4832b5ebcaf4b544c94fd6759
Jeremy P Becker <jpbecker@stanford.edu>
2017-02-06 10:32:02 -0800

    Initial commit

---
+++ src/Hello.java
@@ -0,0 +1,10 @@
+/**
+ * Created by jpbecker on 2/6/2017.
+ */
+public class Hello {
+    public static void main (String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Hello, Jeremy!");
+
+    }
+}
